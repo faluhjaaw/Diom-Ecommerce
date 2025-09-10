38f8aa85937545ce19f11b2e01d9d6d41fd0768f
faluhjaaw <diawmf@ept.sn>
2025-09-10 16:54:22 +0000

    refactor

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.DIC1.projetTrans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
